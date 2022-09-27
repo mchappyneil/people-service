5442da6c7616249f4cfbc2b38a761cc96f4994e8
Neil Mehta <neil@koho.ca>
2022-09-27 15:07:09 -0400

    Comment out PUT

--- server/server.go
+++ server/server.go
@@ -37,29 +37,29 @@ func savePerson(c echo.Context) error {
 	return c.JSON(http.StatusCreated, p)
 }
 
-func updatePersonName(c echo.Context) error {
-	var personPointer *Person
-	var newPerson Person
-	var index int
-	name := c.Param("name")
-	for i, p := range people {
-		if p.Name == name {
-			personPointer = &p
-			index = i
-		}
-	}
-	if personPointer == nil {
-		return c.JSON(http.StatusNotFound, "Person not found")
-	}
-	people = splice(people, index)
-	if err := c.Bind(&newPerson); err != nil {
-		return err
-	}
-	people = append(people, *newPerson)
-	(*personPointer).Name = newPerson.Name
-	return c.JSON(http.StatusOK, *personPointer)
-}
-
+//func updatePersonName(c echo.Context) error {
+//	var personPointer *Person
+//	var newPerson Person
+//	var index int
+//	name := c.Param("name")
+//	for i, p := range people {
+//		if p.Name == name {
+//			personPointer = &p
+//			index = i
+//		}
+//	}
+//	if personPointer == nil {
+//		return c.JSON(http.StatusNotFound, "Person not found")
+//	}
+//	people = splice(people, index)
+//	if err := c.Bind(&newPerson); err != nil {
+//		return err
+//	}
+//	people = append(people, *newPerson)
+//	(*personPointer).Name = newPerson.Name
+//	return c.JSON(http.StatusOK, *personPointer)
+//}
+//
 func deletePerson(c echo.Context) error {
 	var person *Person
 	var index int
@@ -88,7 +88,7 @@ func main() {
 	e.GET("/users/:name", getPerson)
 	e.GET("/users", getPeople)
 	e.POST("/users", savePerson)
-	e.PUT("/users/:name", updatePersonName)
+	//e.PUT("/users/:name", updatePersonName)
 	e.DELETE("/users/:name", deletePerson)
 	e.Logger.Fatal(e.Start(":1323"))
 }
